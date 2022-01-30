5659780691b40c38967eac8de7d6d621b4a3c40d
Camilla Torres <97059336+torrescamilla@users.noreply.github.com>
2022-01-30 14:06:09 -0300

    remove duplicate files

--- src/pages/Home/index 2.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Shopping from '../../components/Shopping'
-// import { Container } from './styles';
-
-function Home() {
-  return <Shopping />;
-}
-
-export default Home;
