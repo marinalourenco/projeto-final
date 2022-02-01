fd1c64a272b38ddf18f0d651facb759e0e012c94
NIK4CI0 <luanavsnicacio@gmail.com>
2022-02-01 20:35:56 -0300

    Login

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,82 +1,45 @@
-import Modal from 'react-modal';
-import { GrClose } from 'react-icons/gr'
-import { Container } from './styles';
-import { useContext } from 'react';
-import { useForm} from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
-import { useHistory } from 'react-router-dom';
-import { AuthContext } from '../../contextApi/AuthProvider';
-import * as yup from 'yup';
+import { useState } from 'react/cjs/react.development';
+import Modal from './componets/Modal';
 
-type Inputs = {
-  email: string,
-  password: string
-};
-
-let schema = yup.object().shape({
-  email: yup.string().email('Informe um email válido').required('Login é obrigatório'),
-  password: yup.string().min(8).required('Senha é obrigatório')
-});
-
-interface LoginModalProps {
-  isOpen: boolean;
-  onRequestClose: () => void;
-}
-
-export function ModalLogin({isOpen, onRequestClose}: LoginModalProps) {
-  const { signIn } = useContext(AuthContext);
-
-  const history = useHistory();
-  const { register, handleSubmit, formState: { errors } } = useForm<Inputs>({
-    resolver: yupResolver(schema)
-  });
+function App() {
+  const [IsModalVisible, setIsModalVisible] = useState(false);
   
-  async function onSubmit(data: Inputs) {
-    console.log(data)
-    await signIn(data);
-    history.push("/home")
-   
-  };
-
-
   return (
-    <Modal 
-      isOpen={isOpen}
-      onRequestClose={onRequestClose}
-      overlayClassName="react-modal-overlay"
-      className="react-modal-content"
-    >
-      <button 
-        className="btnClose" 
-        type="button" 
-        onClick={onRequestClose}
-      >
-        <GrClose 
-          className="iconClose" 
-        />
-      </button>
-      <Container onSubmit={handleSubmit(onSubmit)}>
-        <h2>Faça seu login</h2>
-        
-      
-          <input placeholder="Digite seu email" {...register("email")}/>
-          <p>{errors.email && <span>{errors.email.message}</span>}</p>
+    <div className="App">
 
-          <input type="password" placeholder="Digite sua senha" {...register("password")}/>
-          <p>{errors.password && <span>{errors.password.message}</span>}</p>
+    <button onClick={() => setIsModalVisible(true)}> Open </button>
 
-          <div className="senha">
-            <p>Esqueci minha <a href="">senha</a></p>
+    {IsModalVisible ? (
+    <Modal onClose= {() => setIsModalVisible (false)}> 
+
+<div className='form'>
+        <div className='form_logo'>
+          Lo<span>g</span>o
+        </div>
+        <form className='form_items'>
+          <div className='form_inputs'>
+            <input 
+              type='text'
+              required
+              />
+            <label>username or email</label>
           </div>
-
-          <button type="submit">ENTRAR</button>
-          
-
-          <div className="register">
-            <p>Não tem conta? <a href="">Registre-se</a></p>
+          <div className='form_inputs'>
+            <input
+              type='password'
+              required
+              />
+            <label>password</label>
           </div>
-      
-      </Container>
-    </Modal>
-  )
-}
+          <button className='form_button'>GO</button>
+        </form>
+        <div className='form_other'>
+          <a href='#'>forgot password?</a>
+          <a href='#'>Join Now</a>
+        </div>
+      </div>
+     
+    </Modal> ) : null}
+    </div> )}
+
+export default App; 
