7b757015d3916df2830d11ba05c87e411597f8e1
marinalourenco <marinarquitet@gmail.com>
2022-02-01 23:46:43 -0300

    update

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,16 @@
 import { FaUser, FaShoppingCart } from 'react-icons/fa';
 import { User } from './styles';
 import { useCart } from '../../hooks/useCart';
-import { Link } from 'react-router-dom';
+import { useAuth } from '../../hooks/useAuth';
+import { useModal } from '../../hooks/useModal';
+import { Link, useNavigate } from 'react-router-dom';
 import Logo from "../../assets/logo.png"
 
-export function Header({ onOpenNewRegisterModal, onOpenLoginModal }) {
+export function Header() {
+    const navigate = useNavigate()
     const { cart } = useCart();
+    const { auth, signOut } = useAuth();
+    const { handleOpenLoginModal } = useModal();
     const cartSize = cart?.length;
   
     return (
@@ -22,18 +27,28 @@ export function Header({ onOpenNewRegisterModal, onOpenLoginModal }) {
           </div>
           
           <div className="users">
+            {!auth?(
             <div className="user">
-            <Link to="/profile">
-              <FaUser className="FaUser"/>
-              </Link>
-              Faça seu <button type="button" 
-              onClick={onOpenLoginModal}
+              <FaUser className="FaUser"/> Faça seu <button type="button" 
+                onClick={handleOpenLoginModal}
               >
                 Login
-              </button> ou <button type="button" onClick={onOpenNewRegisterModal}>
+              </button> ou <button type="button" onClick={() => navigate("/profile")}>
                 Cadastre-se
               </button>
             </div>
+            ):(
+              <div className="user">
+              <FaUser className="FaUser"/> Edite o seu 
+              <button type="button" 
+                onClick={() => navigate("/profile")}
+              >
+                Perfil
+              </button> ou prefere <button type="button" onClick={signOut}>
+                Sair
+              </button>
+            </div>
+            )}
   
             <span className="car">
               <FaShoppingCart /> <Link className="cart" to="/cart"> Seu carrinho tem <p className="shop">{cartSize === 1 ? `${cartSize} item` : `${cartSize} itens`}</p></Link>
