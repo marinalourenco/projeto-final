b8ea5986a57562278b2bec78218cfe812c13afb0
Marina Lourenço <71576230+marinalourenco@users.noreply.github.com>
2022-02-03 18:58:24 -0300

    Update styles.js

--- src/components/Shopping/styles.js
+++ src/components/Shopping/styles.js
@@ -13,6 +13,22 @@ export const Container = styled.div`
   flex-direction: column;
 `
 
+export const Loading = styled.div`
+    width: 60%;
+    height: 70%;
+    min-height: 35em;
+    background-color: white;
+    padding-left: 2em;
+    border-radius: 2rem;
+    border-color: ${mixins.colors.secondary}; 
+    border: 2px solid ${mixins.colors.secondary};
+    
+    display: flex;
+    flex-direction: column:
+    
+    box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+`;
+
 export const Content = styled.div`
   margin: 5rem;
   display: flex;
